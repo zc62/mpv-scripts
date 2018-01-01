aba613d720edc432b9bc470743b0136333195cd1
cczzhh <chenzh1993@gmail.com>
2018-01-01 18:31:39 -0500

    Add screenshot-to-clipboard.js
    mpv-player/mpv#5330

---
+++ screenshot-to-clipboard.js
@@ -0,0 +1,55 @@
+// Issue mpv-player/mpv#5330
+// copy screenshot to clipboard, instead of saving to file (Windows only)
+// nircmd is required: http://www.nirsoft.net/utils/nircmd.html
+//
+// script option: ss-2-cb-nircmdc="nircmdc"
+//     Path to nircmdc executable file.
+//     If not set, nircmdc will be searched in Windows PATH variable.
+//
+// Example: mpv file.mkv --script-opts=ss-2-cb-nircmdc="C:\nircmd\nircmdc.exe"
+
+var nircmdc = "nircmdc";
+
+function getOption()
+{
+    var opt = mp.get_opt("ss-2-cb-nircmdc");
+    if (opt)
+        nircmdc = opt;
+}
+getOption();
+
+var ss_file = mp.utils.get_user_path("~~desktop/mpv-ss-2-cb.png");
+
+function ss_2_cb()
+{
+    if (!ss_file)
+        return;
+    mp.commandv("screenshot-to-file", ss_file);
+    mp.utils.subprocess_detached({
+        "args" : [ nircmdc, "clipboard", "copyimage", ss_file ]
+    });
+}
+
+function ss_2_cb_video()
+{
+    if (!ss_file)
+        return;
+    mp.commandv("screenshot-to-file", ss_file, "video");
+    mp.utils.subprocess_detached({
+        "args" : [ nircmdc, "clipboard", "copyimage", ss_file ]
+    });
+}
+
+function ss_2_cb_window()
+{
+    if (!ss_file)
+        return;
+    mp.commandv("screenshot-to-file", ss_file, "window");
+    mp.utils.subprocess_detached({
+        "args" : [ nircmdc, "clipboard", "copyimage", ss_file ]
+    });
+}
+
+mp.add_key_binding("s", "screenshot-to-clipboard", ss_2_cb);
+mp.add_key_binding("S", "screenshot-to-clipboard_video", ss_2_cb_video)
+mp.add_key_binding("ctrl+s", "screenshot-to-clipboard_window", ss_2_cb_window)
