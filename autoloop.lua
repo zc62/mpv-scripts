5253e1f3f404b2e2fadfd76c051bf5cea0777917
CreamyCookie <CreamyCookie@users.noreply.github.com>
2020-08-28 17:13:07 +0200

    Disable save-position-on-quit for files that qualify for looping

--- autoloop.lua
+++ autoloop.lua
@@ -2,6 +2,7 @@
 -- Automatically set loop-file=inf for duration <= given length. Default is 5s
 -- Use autoloop_duration=n in script-opts/autoloop.conf to set your preferred length
 -- Alternatively use script-opts=autoloop-autoloop_duration=n in mpv.conf (takes priority)
+-- Also disables the save-position-on-quit for this file, if it qualifies for looping.
 
 
 require 'mp.options'
@@ -42,6 +43,7 @@ function set_loop()
     -- Loops file if was_loop is false, and file meets requirements
     if not was_loop and duration <= autoloop_duration then
         mp.set_property_native("loop-file", true)
+        mp.command("no-osd set file-local-options/save-position-on-quit no")
         -- Unloops file if was_loop is true, and file does not meet requirements
     elseif was_loop and duration > autoloop_duration then
         mp.set_property_native("loop-file", false)
